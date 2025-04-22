74947edb81e05608251402b151f371346aca66bf
Mujtaba <mujtabarasheed20@gmail.com>
2025-04-22 22:43:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// Primitives
+var myname = "Mudassir";
+var age = 200;
+var weight = 500.3;
+var status = true;
+var n1 = null;
+var salary ;
+
+// console.log("name" , myname);
+// console.log("age" , age);
+// console.log("weight" , weight);
+// console.log("status" , status);
+// console.log("n1" , n1);
+// console.log("salary" , salary);
+
+// Non Primitives 
+// array 
+var data1 = ["Mudassir" , 20 , 10 , 15 , 16 , "absent" , true  , "pass"];
+for(var i = 0; i<data1.length; i++){
+    console.log(data1[i]);
+}
+
+// Object
+// var details = {
+//     name : "mateen",
+//     age : 550,
+//     slary : 50,
+//     weight : 2.2,
+//     country : "Kenya",
+//     height : null 
+// }
+
+// console.log(details.weight);
+
+
+
+
